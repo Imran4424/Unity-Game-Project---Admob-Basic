61a6891e043dce674bd9b17287a248057f87f9cc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-18 20:44:43 +0600

    ad basic

---
+++ Assets/Scripts/AdsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class AdsController : MonoBehaviour
+{
+
+	private RewardBasedVideoAd reward_based;
+
+	void Awake ()
+	{
+		reward_based = RewardBasedVideoAd.Instance;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	private void LoadRewardBasedAd()
+	{
+		#if UNINY_EDITOR
+			string adUnitId = "unused";
+		#elif UNITY_ANDROID = 
+	}
+}
