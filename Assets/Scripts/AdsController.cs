ab40a0950e96eef5e54d65db45511358a8906d40
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-19 05:56:46 +0600

    ad basic

--- Assets/Scripts/AdsController.cs
+++ Assets/Scripts/AdsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GoogleMobileAds;
 using GoogleMobileAds.Api;
 using UnityEngine;
+using System;
 
 public class AdsController : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class AdsController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		LoadRewardBasedAd();
+		LoadRewardBasedAd ();
 	}
 
 	// Update is called once per frame
@@ -26,30 +27,47 @@ public class AdsController : MonoBehaviour
 
 	}
 
-	public void ShowTheadd()
+	public void ShowTheadd ()
 	{
-		if (reward_based.IsLoaded())
+		if (reward_based.IsLoaded ())
 		{
-			reward_based.Show();
+			reward_based.Show ();
 
-			LoadRewardBasedAd();
+			LoadRewardBasedAd ();
 		}
 		else
 		{
-			MonoBehaviour.print("Dude your add's is not loaded yet");
+			MonoBehaviour.print ("Dude your add's is not loaded yet");
 		}
 	}
 
 	private void LoadRewardBasedAd ()
 	{
-		#if UNINY_EDITOR
-			string adUnitId = "unused";
-		#elif UNITY_ANDROID
-			string adUnitId = "ca-app-pub-8350504222422488/7241250290";
-		#else
-			string adUnitId = "unexpected_platform";
-		#endif
+#if UNINY_EDITOR
+		string adUnitId = "unused";
+#elif UNITY_ANDROID
+		string adUnitId = "ca-app-pub-8350504222422488/7241250290";
+#else
+		string adUnitId = "unexpected_platform";
+#endif
 
-		reward_based.LoadAd(new AdRequest.Builder().Build(), adUnitId);
+		reward_based.LoadAd (new AdRequest.Builder ().Build (), adUnitId);
 	}
+
+	public event EventHandler<EventArgs> OnAdLoaded;
+
+	public event EventHandler<AdFailedToLoadEventArgs> OnAdFailedToLoad;
+
+	public event EventHandler<EventArgs> OnAdOpening;
+
+	public event EventHandler<EventArgs> OnAdStarted;
+
+	public event EventHandler<EventArgs> OnAdClosed;
+
+	public event EventHandler<Reward> OnAdRewarded;
+
+	public event EventHandler<EventArgs> OnAdLeavingApplication;
+
+	public event EventHandler<EventArgs> OnAdCompleted;
+
 }
