6f308de8de84f4443547808c003f491c2b1e140e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-21 22:28:56 +0600

    admob basic

--- Assets/Scripts/BannerAd.cs
+++ Assets/Scripts/BannerAd.cs
@@ -17,17 +17,7 @@ public class BannerAd : MonoBehaviour
 	{
 		string adID = "ca-app-pub-8350504222422488/6121177109";
 
-		//***For Testing in the Device***
-		/* AdRequest request = new AdRequest.Builder ()
-			.AddTestDevice (AdRequest.TestDeviceSimulator) // Simulator.
-			.AddTestDevice ("2077ef9a63d2b398840261c8221a0c9b") // My test device.
-			.Build (); */
-
-		//***For Production When Submit App***
-		AdRequest request = new AdRequest.Builder().Build();
-
-		BannerView bannerAd = new BannerView (adID, AdSize.SmartBanner, AdPosition.Bottom);
-		bannerAd.LoadAd (request);
+		
 	}
 
 	// Update is called once per frame
