d8178b8ceb6d6bb128a639520cc724829fd5acd6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-21 22:50:04 +0600

    admob basic

--- Assets/Scripts/BannerAd.cs
+++ Assets/Scripts/BannerAd.cs
@@ -24,14 +24,14 @@ public class BannerAd : MonoBehaviour
 		string adUnitId = "ca-app-pub-8350504222422488/6121177109";
 
 		// Create a 320x50 banner at the top of the screen.
-		bannerView = new BannerView (adUnitId, AdSize.Banner, AdPosition.Top);
+		bannerView = new BannerView (adUnitId, AdSize.Banner, AdPosition.Bottom);
 
 		// Create an empty ad request.
 		AdRequest request = new AdRequest.Builder ().Build ();
 
 		// Load the banner with the request.
 		bannerView.LoadAd (request);
-		
+
 	}
 
 	// Update is called once per frame
