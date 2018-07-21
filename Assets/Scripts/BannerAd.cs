a9dc8539eb98d5f2c912c9a0d455f66bf8f83ace
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-07-21 14:43:21 +0600

    admob basic

--- Assets/Scripts/BannerAd.cs
+++ Assets/Scripts/BannerAd.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
 
-public class BannerAd : MonoBehaviour {
+public class BannerAd : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		
+
 	}
 
-
-	
-	// Update is called once per frame
-	void Update () 
+	private void showBannerAd ()
 	{
-		
+		string adID = "ca-app-pub-3940256099942544/6300978111";
+
+		//***For Testing in the Device***
+		AdRequest request = new AdRequest.Builder ()
+			.AddTestDevice (AdRequest.TestDeviceSimulator) // Simulator.
+			.AddTestDevice ("2077ef9a63d2b398840261c8221a0c9b") // My test device.
+			.Build ();
+
+		//***For Production When Submit App***
+		//AdRequest request = new AdRequest.Builder().Build();
+
+		BannerView bannerAd = new BannerView (adID, AdSize.SmartBanner, AdPosition.Top);
+		bannerAd.LoadAd (request);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
 	}
 }
